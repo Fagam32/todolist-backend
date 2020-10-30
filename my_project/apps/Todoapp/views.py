179e4a61afea82ec15c6ec9e58458deff300719d
Dmitry913 <st077178@student.spbu.ru>
2020-10-30 16:17:38 +0300

    Fix bag 1.0

--- my_project/apps/Todoapp/views.py
+++ my_project/apps/Todoapp/views.py
@@ -1,25 +1,27 @@
-from django.shortcuts import render
-from django.views import View
 import json
 from datetime import datetime
 
 from django.http import JsonResponse, Http404
+from django.views import View
+
 from .models import Todo
 
 
 class ToDoList(View):
 
-    def get(self, request, *args, **kwargs):  # done
-        return JsonResponse(list(Todo.objects.filter().values()), safe=False)  # костыль
+    def get(self, request, *args, **kwargs):
+        all_todos = list(Todo.objects.filter().values())
+        return JsonResponse(all_todos, safe=False)
 
-    def post(self, request, *args, **kwargs):  # done
+    def post(self, request, *args, **kwargs):
         time = datetime.now().replace(microsecond=0)
         todo = json.loads(request.body)
         temp = Todo(text=todo['text'], isDone=False, creationDate=time, lastUpdate=time)
         temp.save()
-        return JsonResponse(list(Todo.objects.filter(creationDate=time).values()), safe=False)  # костыль
+        added_todo = list(Todo.objects.filter(creationDate=time).values())
+        return JsonResponse(added_todo, safe=False)
 
-    def put(self, request, *args, **kwargs):  # done
+    def put(self, request, *args, **kwargs):
         todo = json.loads(request.body)
         try:
             todo_from_db = Todo.objects.get(id=todo['id'])
@@ -27,14 +29,15 @@ class ToDoList(View):
             todo_from_db.isDone = todo['isDone']
             todo_from_db.lastUpdate = datetime.now().replace(microsecond=0)
             todo_from_db.save()
-            return JsonResponse(list(Todo.objects.filter(id=todo['id']).values()))  # костыль
+            updated_todo = list(Todo.objects.filter(id=todo['id']).values())
+            return JsonResponse(updated_todo)  # костыль
         except Todo.DoesNotExist:
-            return Http404()
+            return JsonResponse(None, safe=False, status=404)
 
-    def delete(self, request, *args, **kwargs):  # done
+    def delete(self, request, *args, **kwargs):
         todo = json.loads(request.body)
         try:
             Todo.objects.get(id=todo['id']).delete()
-            return JsonResponse(True, safe=False)
+            return JsonResponse(True, safe=False, status=200)
         except Todo.DoesNotExist:
-            return JsonResponse(False, safe=False)
+            return JsonResponse(False, safe=False, status=404)
