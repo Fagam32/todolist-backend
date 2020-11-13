7459911edcdf291f1b17725585a56a7889afe608
ivolodin <ilya.volodin@telekom.com>
2020-11-13 21:14:13 +0300

    Fixed double login bug

--- my_project/apps/Authorization/views.py
+++ my_project/apps/Authorization/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.models import User
-from django. contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout
 import json
 from django.http import JsonResponse
 from django.db.utils import IntegrityError
@@ -14,7 +14,8 @@ class Registration(View):
         getting_data = json.loads(request.body)
         # TODO remove password from cheсking on uniq
         try:
-            User.objects.create_user(username=getting_data['email'], first_name=getting_data['name'], last_name=getting_data['surname'],
+            User.objects.create_user(username=getting_data['email'], first_name=getting_data['name'],
+                                     last_name=getting_data['surname'],
                                      email=getting_data['email'], password=getting_data['password'])
             return JsonResponse("", safe=False, status=200)
         except IntegrityError:  # если какой-то параметр не уникален
@@ -25,15 +26,16 @@ class Registration(View):
 class Authorize(View):
 
     def post(self, request, *args, **kwargs):
-        getting_data = json.loads(request.body)
-        user = authenticate(request, username=getting_data['email'], password=getting_data['password'])
+        credentials = json.loads(request.body)
+        user = authenticate(request, username=credentials['email'], password=credentials['password'])
         if user:
-            # try:
-                # login(request, user)
-            token = Token.objects.create(user=user)
-            response = {"roles": ["USER"], "token": "Bearer " + token.key}
-            return JsonResponse(response, safe=False, status=200)
-            # except IntegrityError:
-            #     return JsonResponse("User has logged in already", safe=False, status=200)
+            token = Token.objects.get(user=user)
+            if token:
+                response = {"roles": ["USER"], "token": "Bearer " + token.key}
+                return JsonResponse(response, safe=False, status=200)
+            else:
+                token = Token.objects.create(user=user)
+                response = {"roles": ["USER"], "token": "Bearer " + token.key}
+                return JsonResponse(response, safe=False, status=200)
         else:
             return JsonResponse("User no found", safe=False, status=404)
