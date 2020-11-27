65d7bad65b7f3bc529acdbd01a52155f71446b7a
Dmitry913 <st077178@student.spbu.ru>
2020-11-27 16:39:06 +0300

    update method check of email

--- my_project/apps/Authorization/views.py
+++ my_project/apps/Authorization/views.py
@@ -7,13 +7,7 @@ from django.http import JsonResponse
 from django.db.utils import IntegrityError
 from rest_framework.authtoken.models import Token
 import sys
-
-def check_email(string):
-    if '@' not in string:
-        return False
-    else:
-        return (' ' not in string) and ('@.' not in string) and (string.find('.', string.find('@')) != -1)
-
+from email_validator import validate_email, EmailNotValidError
 
 
 class Registration(View):
@@ -21,13 +15,14 @@ class Registration(View):
     def post(self, request, *args, **kwargs):
         getting_data = json.loads(request.body)
         try:
-            if not check_email(getting_data['email']):
-                return JsonResponse("", safe=False, status=400)
+            email = validate_email(getting_data['email'])
             User.objects.create_user(username=getting_data['email'], first_name=getting_data['name'],
                                      last_name=getting_data['surname'],
                                      email=getting_data['email'], password=getting_data['password'])
             return JsonResponse("", safe=False, status=200)
         except IntegrityError:  # если какой-то параметр не уникален
+            return JsonResponse("", safe=False, status=400)
+        except EmailNotValidError:
             return JsonResponse("", safe=False, status=400)
 
 
