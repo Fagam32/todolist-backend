342da63f4be6916b895fcf2be6b10173803fe0eb
ivolodin <ilya.volodin@telekom.com>
2020-12-05 16:21:15 +0300

    DB

--- my_project/settings.py
+++ my_project/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import sys
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # from my_project.apps import Todoapp
 from my_project.apps import Authorization
+
+django_heroku.settings(locals())
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(__file__)
@@ -92,11 +94,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'my_project.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/s   ettings/#databases
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
